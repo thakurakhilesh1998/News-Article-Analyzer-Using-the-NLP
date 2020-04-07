aecfe0657ef410e4e69754715727eeed94c2db95
thakurakhilesh1998 <39956027+thakurakhilesh1998@users.noreply.github.com>
2020-04-07 14:17:22 +0530

    third commit

---
+++ src/test/formHandler.spec.js
@@ -0,0 +1,5 @@
+import { handleSubmit } from '../client/js/formHandler'
+import "babel-polyfill"
+describe('testing the server',()=>{
+    except(true).toBe(true);
+})
--- webpack.dev.js
+++ webpack.dev.js
@@ -5,6 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
+    
     output:{
       libraryTarget:'var',
       library:'Client'
